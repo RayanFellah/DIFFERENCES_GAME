d1ed50d4df286f92c46aacc404ee4a0845856aad
leZ404 <zied.kaabi2001@gmail.com>
2023-02-09 14:55:26 -0500

    tests for game cards grid

--- client/src/app/components/game-cards-grid/game-cards-grid.component.spec.ts
+++ client/src/app/components/game-cards-grid/game-cards-grid.component.spec.ts
@@ -26,36 +26,45 @@ describe('GameCardsGridComponent', () => {
         expect(component.games).toEqual(games);
     });
 
-    it('should have "gridIndexStart" and "gridIndexEnd" properties set to 0 and 4 respectively', () => {
-        expect(component.gridIndexStart).toEqual(0);
-        expect(component.gridIndexEnd).toEqual(4);
-    });
-
-    it('should have a property "gridGames" which is set to a slice of the games array having 0 as a default start Index and 4 as a default end Index', () => {
+    it('should have a property "gridGames"  that is set to a slice of the games array having 0 as a start index and 4 ads an end one', () => {
         expect(component.gridGames).toBeDefined();
         expect(component.gridGames.length).toEqual(4);
         expect(component.gridGames).toEqual(games.slice(0, 4));
     });
 
-    it('nextGrid() should update gridGames to the new slice of games by increasing its start & end index by 4 ', () => {
+    it('should have  a property "gridIndexStart"   set to 0', () => {
+        expect(component.gridIndexStart).toEqual(0);
+    });
+
+    it('should have  a property "gridIndexEnd"   set to 4', () => {
+        expect(component.gridIndexEnd).toEqual(4);
+    });
+
+    it('nextGrid() should increase gridIndexStart and gridIndexEnd by 4 and update gridGames to the new slice of games', () => {
         component.nextGrid();
         expect(component.gridIndexStart).toEqual(4);
         expect(component.gridIndexEnd).toEqual(8);
         expect(component.gridGames).toEqual(games.slice(4, 8));
     });
 
-    it('prevGrid() should update gridGames to the new slice of games by decreasing its start & end index by 4', () => {
+    it('prevGrid() should decrease gridIndexStart and gridIndexEnd a by 4 and update gridGames to the new slice of games', () => {
         component.prevGrid();
         expect(component.gridIndexStart).toEqual(0);
         expect(component.gridIndexEnd).toEqual(4);
         expect(component.gridGames).toEqual(games.slice(0, 4));
     });
 
-    it('deleteGame(index) should remove the concerned game tile and return the updated games List ', () => {
+    it('deleteGame() should remove the game at the specified index from the games array, and update gridGames to the new slice of games', () => {
         const indexToDelete = 1;
-        const defaultGridLength = component.games.length;
+        const defaultGamesLength = component.games.length;
         component.deleteGame(indexToDelete);
-        expect(component.games.length).toEqual(defaultGridLength - 1);
+        expect(component.games.length).toEqual(defaultGamesLength - 1);
         expect(component.gridGames).toEqual(component.games.slice(component.gridIndexStart, component.gridIndexEnd));
     });
+
+    it('playGame() should not throw an error', () => {});
+
+    it('createMultiGame() should not throw an error', () => {});
+
+    it('resetScores() should not throw an error', () => {});
 });
