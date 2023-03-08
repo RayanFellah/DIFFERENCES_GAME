6884ae73136bba885f160d32f15d3668f518799d
leZ404 <zied.kaabi2001@gmail.com>
2023-03-08 13:51:16 -0500

    test config buttons

--- client/src/app/components/config-buttons/config-buttons.component.spec.ts
+++ client/src/app/components/config-buttons/config-buttons.component.spec.ts
@@ -1,23 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
 import { ConfigButtonsComponent } from './config-buttons.component';
 
 describe('ConfigButtonsComponent', () => {
-  let component: ConfigButtonsComponent;
-  let fixture: ComponentFixture<ConfigButtonsComponent>;
+    let component: ConfigButtonsComponent;
+    let fixture: ComponentFixture<ConfigButtonsComponent>;
+    let mockDialog: jasmine.SpyObj<MatDialog>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ConfigButtonsComponent ]
-    })
-    .compileComponents();
+    beforeEach(() => {
+        mockDialog = jasmine.createSpyObj('MatDialog', ['open']);
 
-    fixture = TestBed.createComponent(ConfigButtonsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        TestBed.configureTestingModule({
+            declarations: [ConfigButtonsComponent],
+            providers: [{ provide: MatDialog, useValue: mockDialog }],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(ConfigButtonsComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe('openConstants()', () => {
+        it('should open the constants dialog', () => {
+            const dialogRefSpyObj = jasmine.createSpyObj({ afterClosed: of({}), close: null });
+            mockDialog.open.and.returnValue(dialogRefSpyObj);
+
+            component.openConstants();
+
+            expect(mockDialog.open).toHaveBeenCalled();
+        });
+    });
 });
