e5e676ce23af6d93912c15283784487d4282aaa6
leZ404 <zied.kaabi2001@gmail.com>
2023-03-09 14:44:43 -0500

    imageDialog + fileUpload (still need cov)

--- client/src/app/components/file-upload/file-upload.component.spec.ts
+++ client/src/app/components/file-upload/file-upload.component.spec.ts
@@ -1,29 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { BmpVerificationService } from '@app/services/bmp-verification.service';
-import { SnackBarService } from '@app/services/snack-bar.service';
 import { FileUploadComponent } from './file-upload.component';
 
 describe('FileUploadComponent', () => {
     let component: FileUploadComponent;
     let fixture: ComponentFixture<FileUploadComponent>;
-    let bmpVerificationService: BmpVerificationService;
-    let snackBarService: SnackBarService;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [FileUploadComponent],
             imports: [FormsModule, MatSnackBarModule],
-            providers: [BmpVerificationService, SnackBarService],
+            declarations: [FileUploadComponent],
+            providers: [{ provide: HTMLCanvasElement, useValue: {} }],
         }).compileComponents();
     });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(FileUploadComponent);
         component = fixture.componentInstance;
-        bmpVerificationService = TestBed.inject(BmpVerificationService);
-        snackBarService = TestBed.inject(SnackBarService);
         fixture.detectChanges();
     });
 
@@ -31,47 +25,71 @@ describe('FileUploadComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should call onChange function when a file is selected', () => {
+    it('should set file when onFileChange is called', () => {
+        const mockFile = new File([''], 'filename');
         spyOn(component, 'onChange');
-        const file = new File([''], 'test.jpg', { type: 'image/jpeg' });
-        const inputElement = fixture.nativeElement.querySelector('input[type="file"]');
-        const event = new Event('change');
-        inputElement.files = [file];
-        inputElement.dispatchEvent(event);
+        spyOn(component['bmpVerificationService'], 'verifyImage').and.returnValue(true);
+        const input = fixture.debugElement.nativeElement.querySelector('input[type="file"]');
+        input.dispatchEvent(new Event('change'));
         fixture.detectChanges();
-        expect(component.onChange).toHaveBeenCalledWith(file);
+        expect(component.file).toEqual(mockFile); // a verifier
+        expect(component.fileName).toEqual('filename'); // a verifier & changer
+        expect(component.onChange).toHaveBeenCalled();
     });
 
-    it('should not call onChange function if file is not a valid BMP image', () => {
+    it('should not set file when onFileChange is called and image is invalid', () => {
         spyOn(component, 'onChange');
-        spyOn(bmpVerificationService, 'verifyImage').and.returnValue(false);
-        spyOn(snackBarService, 'openSnackBar');
-        const file = new File([''], 'test.jpg', { type: 'image/jpeg' });
-        const inputElement = fixture.nativeElement.querySelector('input[type="file"]');
-        const event = new Event('change');
-        inputElement.files = [file];
-        inputElement.dispatchEvent(event);
+        spyOn(component['bmpVerificationService'], 'verifyImage').and.returnValue(false);
+        spyOn(component['snackBar'], 'openSnackBar');
+        const input = fixture.debugElement.nativeElement.querySelector('input[type="file"]');
+        input.dispatchEvent(new Event('change'));
         fixture.detectChanges();
+        expect(component.file).toBeUndefined();
+        expect(component.fileName).toBeUndefined();
         expect(component.onChange).not.toHaveBeenCalled();
-        expect(snackBarService.openSnackBar).toHaveBeenCalled();
     });
 
-    it('should clear file when remove button is clicked', () => {
-        const file = new File([''], 'test.jpg', { type: 'image/jpeg' });
-        component.writeValue(file);
-        fixture.detectChanges();
-        const removeButton = fixture.nativeElement.querySelector('button');
-        removeButton.click();
-        fixture.detectChanges();
+    it('should reset file when onRemoveFile is called', () => {
+        const mockFile = new File([''], 'filename');
+        component.file = mockFile;
+        component.fileName = 'filename';
+        spyOn(component, 'onChange');
+        component.onRemoveFile();
         expect(component.file).toBeNull();
         expect(component.fileName).toEqual('');
-        expect(component.touched).toBeTruthy();
+        expect(component.onChange).toHaveBeenCalled();
     });
 
-    it('should be disabled when disabled is set to true', () => {
+    it('should call onTouched when markAsTouched is called', () => {
+        spyOn(component, 'onTouched');
+        component.markAsTouched();
+        expect(component.touched).toBeTruthy();
+        expect(component.onTouched).toHaveBeenCalled();
+    });
+
+    it('should set disabled state when setDisabledState is called', () => {
         component.setDisabledState(true);
-        fixture.detectChanges();
-        const inputElement = fixture.nativeElement.querySelector('input[type="file"]');
-        expect(inputElement.disabled).toBeTruthy();
+        expect(component.disabled).toBeTruthy();
+        component.setDisabledState(false);
+        expect(component.disabled).toBeFalsy();
+    });
+
+    it('should call registerOnChange when registerOnChange is called', () => {
+        const mockOnChange = jasmine.createSpy('mockOnChange');
+        component.registerOnChange(mockOnChange);
+        expect(component.onChange).toEqual(mockOnChange);
+    });
+
+    it('should call registerOnTouched when registerOnTouched is called', () => {
+        const mockOnTouched = jasmine.createSpy('mockOnTouched');
+        component.registerOnTouched(mockOnTouched);
+        expect(component.onTouched).toEqual(mockOnTouched);
+    });
+
+    it('should write value when writeValue is called with file', () => {
+        const mockFile = new File([''], 'filename');
+        component.writeValue(mockFile);
+        expect(component.file).toEqual(mockFile);
+        expect(component.fileName).toEqual('filename');
     });
 });
--- client/src/app/components/image-dialog/image-dialog.component.spec.ts
+++ client/src/app/components/image-dialog/image-dialog.component.spec.ts
@@ -1,23 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ImageDialogComponent } from './image-dialog.component';
 
 describe('ImageDialogComponent', () => {
-  let component: ImageDialogComponent;
-  let fixture: ComponentFixture<ImageDialogComponent>;
+    let component: ImageDialogComponent;
+    let fixture: ComponentFixture<ImageDialogComponent>;
+    let domSanitizer: DomSanitizer;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ImageDialogComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ImageDialogComponent],
+            providers: [
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: { imageUrl: 'test-image-url' },
+                },
+            ],
+        }).compileComponents();
+    });
 
-    fixture = TestBed.createComponent(ImageDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ImageDialogComponent);
+        component = fixture.componentInstance;
+        domSanitizer = TestBed.inject(DomSanitizer);
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should set imageUrl', () => {
+        spyOn(domSanitizer, 'bypassSecurityTrustUrl');
+        fixture.detectChanges();
+        expect(domSanitizer.bypassSecurityTrustUrl).toHaveBeenCalledWith('data:image/bmp;base64,test-image-url');
+        expect(component.imageUrl).toBeDefined();
+    });
 });
