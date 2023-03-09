b20d557e128fcb6aaef3b04acbbe6355fab9d20e
leZ404 <zied.kaabi2001@gmail.com>
2023-03-08 21:52:13 -0500

    tests

--- client/src/app/components/differences-found/differences-found.component.spec.ts
+++ client/src/app/components/differences-found/differences-found.component.spec.ts
@@ -1,23 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DifferencesFoundService } from '@app/services/differences-found.service';
+import { Subject, Subscription } from 'rxjs';
 import { DifferencesFoundComponent } from './differences-found.component';
 
 describe('DifferencesFoundComponent', () => {
-  let component: DifferencesFoundComponent;
-  let fixture: ComponentFixture<DifferencesFoundComponent>;
+    let component: DifferencesFoundComponent;
+    let fixture: ComponentFixture<DifferencesFoundComponent>;
+    let differencesFoundServiceSpy: jasmine.SpyObj<DifferencesFoundService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DifferencesFoundComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        differencesFoundServiceSpy = jasmine.createSpyObj('DifferencesFoundService', ['getAttributeSubject']);
+        await TestBed.configureTestingModule({
+            declarations: [DifferencesFoundComponent],
+            providers: [{ provide: DifferencesFoundService, useValue: differencesFoundServiceSpy }],
+        }).compileComponents();
+    });
 
-    fixture = TestBed.createComponent(DifferencesFoundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DifferencesFoundComponent);
+        component = fixture.componentInstance;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should initialize differences array with the correct length', () => {
+        const numberOfDifferences = 3;
+        differencesFoundServiceSpy.numberOfDifferences = numberOfDifferences;
+        fixture.detectChanges();
+        expect(component.differences).toEqual(new Array(numberOfDifferences).fill(false));
+    });
+
+    it('should mark the correct difference as found when a difference is found', () => {
+        const value = 1;
+        differencesFoundServiceSpy.getAttributeSubject.and.returnValue(new Subject<number>());
+        fixture.detectChanges();
+        differencesFoundServiceSpy.getAttributeSubject().next(value);
+        expect(component.differences[value]).toBeTrue();
+    });
+
+    it('should unsubscribe from differencesFoundSubscription on destroy', () => {
+        const unsubscribeSpy = jasmine.createSpyObj<Subscription>('unsubscribe', ['unsubscribe']);
+        differencesFoundServiceSpy.getAttributeSubject.and.returnValue(new Subject<number>());
+        fixture.detectChanges();
+        component.ngOnDestroy();
+        expect(unsubscribeSpy.unsubscribe).toHaveBeenCalled();
+    });
 });
--- client/src/app/pages/configuration-page/configuration-page.component.spec.ts
+++ client/src/app/pages/configuration-page/configuration-page.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ConfigButtonsComponent } from '../../components/config-buttons/config-buttons.component';
+import { GameCardGridComponent } from '../../components/game-card-grid/game-card-grid.component';
+import { HeaderComponent } from '../../components/header/header.component';
 import { ConfigurationPageComponent } from './configuration-page.component';
 
 describe('ConfigurationPageComponent', () => {
@@ -8,9 +10,11 @@ describe('ConfigurationPageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [ConfigurationPageComponent],
+            declarations: [ConfigurationPageComponent, HeaderComponent, ConfigButtonsComponent, GameCardGridComponent],
         }).compileComponents();
+    });
 
+    beforeEach(() => {
         fixture = TestBed.createComponent(ConfigurationPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -18,5 +22,23 @@ describe('ConfigurationPageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should display a header component with the page title "Configuration"', () => {
+        const header = fixture.nativeElement.querySelector('app-header');
+        expect(header).toBeTruthy();
+        expect(header.pageTitle).toBe('Configuration');
+    });
+
+    it('should display a config buttons component', () => {
+        const configButtons = fixture.nativeElement.querySelector('app-config-buttons');
+        expect(configButtons).toBeTruthy();
+    });
+
+    it('should display a game card grid component with the id "grid" and isConfig set to true', () => {
+        const gameCardGrid = fixture.nativeElement.querySelector('app-game-card-grid');
+        expect(gameCardGrid).toBeTruthy();
+        expect(gameCardGrid.id).toBe('grid');
+        expect(gameCardGrid.isConfig).toBe(true);
     });
 });
