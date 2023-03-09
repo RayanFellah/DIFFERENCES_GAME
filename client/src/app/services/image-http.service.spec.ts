5ecead1ea7fa0d37f9ab7f67e3331c6a7bd5c63e
DrRadZ <radwan_rahman@rocketmail.com>
2023-03-09 15:14:23 -0500

    mainly snack-bar completed

--- client/src/app/services/image-http.service.spec.ts
+++ client/src/app/services/image-http.service.spec.ts
@@ -1,16 +1,50 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { ImageHttpService } from './image-http.service';
 
 describe('ImageHttpService', () => {
-  let service: ImageHttpService;
+    let service: ImageHttpService;
+    let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ImageHttpService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [ImageHttpService],
+        });
+        service = TestBed.inject(ImageHttpService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should get image successfully', () => {
+        const fileName = 'test.jpg';
+        const mockResponse = new Blob(['test data'], { type: 'image/jpeg' });
+
+        service.getImage(fileName).subscribe((response: Blob) => {
+            expect(response).toEqual(mockResponse);
+        });
+
+        const request = httpMock.expectOne(`${service['baseUrl']}/image/${fileName}`);
+        expect(request.request.method).toBe('GET');
+        request.flush(mockResponse);
+    });
+
+    it('should compare images successfully', () => {
+        const sheetForm = new FormData();
+        sheetForm.append('originalImage', new File(['original image'], 'original.jpg'));
+        sheetForm.append('modifiedImage', new File(['modified image'], 'modified.jpg'));
+        const mockResponse = { differences: 5 };
+
+        service.getDifferences(sheetForm).subscribe((response: unknown) => {
+            expect(response.body).toEqual(mockResponse);
+        });
+
+        const request = httpMock.expectOne(`${service['baseUrl']}/image/compare`);
+        expect(request.request.method).toBe('POST');
+        expect(request.request.body).toEqual(sheetForm);
+        request.flush(mockResponse);
+    });
 });
--- client/src/app/services/sheet-http.service.spec.ts
+++ client/src/app/services/sheet-http.service.spec.ts
@@ -1,16 +1,103 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { Sheet } from '@common/sheet';
 import { SheetHttpService } from './sheet-http.service';
 
 describe('SheetHttpService', () => {
-  let service: SheetHttpService;
+    let service: SheetHttpService;
+    let httpMock: HttpTestingController;
+    const testUrl = 'https://example.com';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SheetHttpService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [SheetHttpService, { provide: 'SERVER_URL', useValue: testUrl }],
+        });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        service = TestBed.inject(SheetHttpService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should get all sheets successfully', () => {
+        const mockResponse: Sheet[] = [
+            {
+                _id: '1',
+                title: 'Sheet 1',
+                originalImagePath: '',
+                modifiedImagePath: '',
+                difficulty: '',
+                radius: 0,
+                topPlayer: '',
+                differences: 0,
+            },
+        ];
+
+        service.getAllSheets().subscribe((sheets: Sheet[]) => {
+            expect(sheets).toEqual(mockResponse);
+        });
+
+        const req = httpMock.expectOne(`${testUrl}/sheet`);
+        expect(req.request.method).toBe('GET');
+        req.flush(mockResponse);
+    });
+
+    it('should create a new sheet successfully', () => {
+        const mockResponse = { id: '1', name: 'New Sheet' };
+        const mockForm = new FormData();
+
+        service.createSheet(mockForm).subscribe((res) => {
+            expect(res).toEqual(mockResponse);
+        });
+
+        const req = httpMock.expectOne(`${testUrl}/sheet`);
+        expect(req.request.method).toBe('POST');
+        expect(req.request.body).toBe(mockForm);
+        req.flush(mockResponse);
+    });
+
+    it('should delete a sheet successfully', () => {
+        const mockId = '1';
+
+        service.deleteSheet(mockId).subscribe((res) => {
+            expect(res).toBeUndefined();
+        });
+
+        const req = httpMock.expectOne(`${testUrl}/sheet/${mockId}`);
+        expect(req.request.method).toBe('DELETE');
+        req.flush(null);
+    });
+
+    it('should get a sheet by id successfully', () => {
+        const mockId = '1';
+        const mockResponse = { id: mockId, name: 'Sheet 1' };
+
+        service.getSheet(mockId).subscribe((sheet) => {
+            expect(sheet).toEqual(mockResponse);
+        });
+
+        const req = httpMock.expectOne(`${testUrl}/sheet/${mockId}`);
+        expect(req.request.method).toBe('GET');
+        req.flush(mockResponse);
+    });
+
+    it('should handle errors correctly', () => {
+        const mockError = new Error('Test error');
+        const errorHandlerSpy = jasmine.createSpy('errorHandler');
+
+        service.getAllSheets().subscribe(() => {}, errorHandlerSpy);
+
+        const req = httpMock.expectOne(`${testUrl}/sheet`);
+        expect(req.request.method).toBe('GET');
+        req.flush(null, { status: 500, statusText: 'Internal Server Error' });
+
+        expect(errorHandlerSpy).toHaveBeenCalledWith(mockError);
+    });
 });
