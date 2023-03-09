57ca599fd3f4236b87787d4cd21786383e21f774
leZ404 <zied.kaabi2001@gmail.com>
2023-03-08 21:09:39 -0500

    resolving error

--- client/src/app/components/dialogue/dialog.component.spec.ts
+++ client/src/app/components/dialogue/dialog.component.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialog } from '@angular/material/dialog';
+import { ImageDialogComponent } from '@app/components/image-dialog/image-dialog.component';
+import { ConstantsDialogComponent } from '../constants-dialog/constants-dialog.component';
 import { DialogComponent } from './dialog.component';
 
 describe('DialogComponent', () => {
     let component: DialogComponent;
     let fixture: ComponentFixture<DialogComponent>;
+    let mockMatDialog: any;
 
     beforeEach(async () => {
+        mockMatDialog = {
+            open: jasmine.createSpy('open'),
+        };
+
         await TestBed.configureTestingModule({
             declarations: [DialogComponent],
+            providers: [{ provide: MatDialog, useValue: mockMatDialog }],
         }).compileComponents();
+    });
 
+    beforeEach(() => {
         fixture = TestBed.createComponent(DialogComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -18,5 +28,27 @@ describe('DialogComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    describe('openImageDialog', () => {
+        it('should open image dialog with provided URL and dimensions', () => {
+            const imageUrl = 'https://example.com/image.jpg';
+            component.openImageDialog(imageUrl);
+            expect(mockMatDialog.open).toHaveBeenCalledWith(ImageDialogComponent, {
+                data: { imageUrl },
+                maxWidth: jasmine.any(String),
+                maxHeight: jasmine.any(String),
+            });
+        });
+    });
+
+    describe('openConstantsDialog', () => {
+        it('should open constants dialog', () => {
+            component.openConstantsDialog();
+            expect(mockMatDialog.open).toHaveBeenCalledWith(ConstantsDialogComponent, {
+                data: {},
+                panelClass: 'custom-modalbox',
+            });
+        });
     });
 });
--- client/src/app/components/file-upload/file-upload.component.spec.ts
+++ client/src/app/components/file-upload/file-upload.component.spec.ts
@@ -1,22 +1,77 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BmpVerificationService } from '@app/services/bmp-verification.service';
+import { SnackBarService } from '@app/services/snack-bar.service';
 import { FileUploadComponent } from './file-upload.component';
 
 describe('FileUploadComponent', () => {
     let component: FileUploadComponent;
     let fixture: ComponentFixture<FileUploadComponent>;
+    let bmpVerificationService: BmpVerificationService;
+    let snackBarService: SnackBarService;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [FileUploadComponent],
+            imports: [FormsModule, MatSnackBarModule],
+            providers: [BmpVerificationService, SnackBarService],
         }).compileComponents();
+    });
 
+    beforeEach(() => {
         fixture = TestBed.createComponent(FileUploadComponent);
         component = fixture.componentInstance;
+        bmpVerificationService = TestBed.inject(BmpVerificationService);
+        snackBarService = TestBed.inject(SnackBarService);
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should call onChange function when a file is selected', () => {
+        spyOn(component, 'onChange');
+        const file = new File([''], 'test.jpg', { type: 'image/jpeg' });
+        const inputElement = fixture.nativeElement.querySelector('input[type="file"]');
+        const event = new Event('change');
+        inputElement.files = [file];
+        inputElement.dispatchEvent(event);
+        fixture.detectChanges();
+        expect(component.onChange).toHaveBeenCalledWith(file);
+    });
+
+    it('should not call onChange function if file is not a valid BMP image', () => {
+        spyOn(component, 'onChange');
+        spyOn(bmpVerificationService, 'verifyImage').and.returnValue(false);
+        spyOn(snackBarService, 'openSnackBar');
+        const file = new File([''], 'test.jpg', { type: 'image/jpeg' });
+        const inputElement = fixture.nativeElement.querySelector('input[type="file"]');
+        const event = new Event('change');
+        inputElement.files = [file];
+        inputElement.dispatchEvent(event);
+        fixture.detectChanges();
+        expect(component.onChange).not.toHaveBeenCalled();
+        expect(snackBarService.openSnackBar).toHaveBeenCalled();
+    });
+
+    it('should clear file when remove button is clicked', () => {
+        const file = new File([''], 'test.jpg', { type: 'image/jpeg' });
+        component.writeValue(file);
+        fixture.detectChanges();
+        const removeButton = fixture.nativeElement.querySelector('button');
+        removeButton.click();
+        fixture.detectChanges();
+        expect(component.file).toBeNull();
+        expect(component.fileName).toEqual('');
+        expect(component.touched).toBeTruthy();
+    });
+
+    it('should be disabled when disabled is set to true', () => {
+        component.setDisabledState(true);
+        fixture.detectChanges();
+        const inputElement = fixture.nativeElement.querySelector('input[type="file"]');
+        expect(inputElement.disabled).toBeTruthy();
     });
 });
