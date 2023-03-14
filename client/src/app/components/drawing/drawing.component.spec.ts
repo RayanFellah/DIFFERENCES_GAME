2f821fa8165736aa1bf15144fe8cf4c4dd2e2aba
Ahmed El Shami <aelshami25@gmail.com>
2023-03-14 13:26:51 -0400

    spect.ts fixed errors

--- client/src/app/components/drawing/drawing.component.spec.ts
+++ client/src/app/components/drawing/drawing.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DrawingComponent } from './drawing.component';
 
 describe('DrawingComponent', () => {
-  let component: DrawingComponent;
-  let fixture: ComponentFixture<DrawingComponent>;
+    let component: DrawingComponent;
+    let fixture: ComponentFixture<DrawingComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DrawingComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DrawingComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DrawingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DrawingComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/services/sheet-http.service.spec.ts
+++ client/src/app/services/sheet-http.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { SheetHttpService } from './sheet-http.service';
 
 describe('SheetHttpService', () => {
-  let service: SheetHttpService;
+    let service: SheetHttpService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SheetHttpService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(SheetHttpService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
