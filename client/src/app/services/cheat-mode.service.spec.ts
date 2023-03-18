e5c57c97695f523a4700c8cf993cf3c022857234
leZ404 <zied.kaabi2001@gmail.com>
2023-03-18 17:28:37 -0400

    fixing cheat spec

--- client/src/app/services/cheat-mode.service.spec.ts
+++ client/src/app/services/cheat-mode.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CheatModeService } from './cheat-mode.service';
+import { GameHttpService } from './game-http.service';
 
 describe('CheatModeService', () => {
     let service: CheatModeService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({ providers: [{ provide: GameHttpService, useValue: {} }] });
         service = TestBed.inject(CheatModeService);
     });
 
