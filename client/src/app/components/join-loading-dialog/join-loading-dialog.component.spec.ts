8c9eeb86887849940a884aa171e2ae065d1212fe
leZ404 <zied.kaabi2001@gmail.com>
2023-03-19 22:56:49 -0400

    join 100%

--- client/src/app/components/join-loading-dialog/join-loading-dialog.component.spec.ts
+++ client/src/app/components/join-loading-dialog/join-loading-dialog.component.spec.ts
@@ -1,22 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DialogService } from '@app/services/dialog-service/dialog.service';
 import { JoinLoadingDialogComponent } from './join-loading-dialog.component';
 
 describe('JoinLoadingDialogComponent', () => {
     let component: JoinLoadingDialogComponent;
     let fixture: ComponentFixture<JoinLoadingDialogComponent>;
+    let dialogService: DialogService;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [JoinLoadingDialogComponent],
+            providers: [DialogService],
         }).compileComponents();
+    });
 
+    beforeEach(() => {
         fixture = TestBed.createComponent(JoinLoadingDialogComponent);
         component = fixture.componentInstance;
+        dialogService = TestBed.inject(DialogService);
+        spyOn(dialogService, 'emitJoinCancellation').and.callFake(() => {});
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should emit join cancellation', () => {
+        component.cancelJoin();
+        expect(dialogService.emitJoinCancellation).toHaveBeenCalled();
     });
 });
