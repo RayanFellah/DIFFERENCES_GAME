8443048175ca539e936d0512bbaa03ae9a7d4bc1
Booskander <skander.hannachi@polymtl.ca>
2023-03-27 23:26:54 -0400

    starting the logic

---
+++ client/src/app/services/time-limit-mode.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TimeLimitModeService } from './time-limit-mode.service';
+
+describe('TimeLimitModeService', () => {
+  let service: TimeLimitModeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TimeLimitModeService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
