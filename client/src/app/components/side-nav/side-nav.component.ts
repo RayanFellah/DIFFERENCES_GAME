c5b6c8732c1ab0bbd578902641771e7561f286fd
Booskander <skander.hannachi@polymtl.ca>
2023-03-27 21:59:26 -0400

    lint fix

--- client/src/app/components/side-nav/side-nav.component.ts
+++ client/src/app/components/side-nav/side-nav.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
     selector: 'app-side-nav',
     templateUrl: './side-nav.component.html',
     styleUrls: ['./side-nav.component.scss'],
 })
-export class SideNavComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
-}
+export class SideNavComponent {}
