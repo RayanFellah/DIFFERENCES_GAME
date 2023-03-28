551088553e989a04a17410e491fba09b87c01eba
Booskander <skander.hannachi@polymtl.ca>
2023-03-27 23:27:33 -0400

    some lint

--- client/src/app/components/side-nav/side-nav.component.spec.ts
+++ client/src/app/components/side-nav/side-nav.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SideNavComponent } from './side-nav.component';
 
 describe('SideNavComponent', () => {
-  let component: SideNavComponent;
-  let fixture: ComponentFixture<SideNavComponent>;
+    let component: SideNavComponent;
+    let fixture: ComponentFixture<SideNavComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SideNavComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SideNavComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(SideNavComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(SideNavComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/services/time-limit-mode.service.spec.ts
+++ client/src/app/services/time-limit-mode.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { TimeLimitModeService } from './time-limit-mode.service';
 
 describe('TimeLimitModeService', () => {
-  let service: TimeLimitModeService;
+    let service: TimeLimitModeService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TimeLimitModeService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(TimeLimitModeService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
